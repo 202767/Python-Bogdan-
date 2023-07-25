85750ed3d841e756f1f17fa2a61d2a66c3ead84e
Anna Chesnokova <chesnokova33@list.ru>
2023-07-25 11:09:42 +0300

    add Task 8

---
+++ задача 8(аргументы).py
@@ -0,0 +1,11 @@
+def merge_list_to_dict(list_one, list_two):
+    new_zip = zip(list_one, list_two)
+    new_dict = dict(new_zip)
+    return (new_dict)
+
+
+list_one = ['asd', 23, True]
+list_two = [1 + 4j, 2.5, ('sfsv', 'wsvs')]
+
+print(merge_list_to_dict(list_one=list_one, list_two=list_two))
+print(merge_list_to_dict(list_one, list_two=list_two))
