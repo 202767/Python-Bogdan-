183d1992cdf7e839d45197ffe2e3b3386ff8c498
Anna Chesnokova <chesnokova33@list.ru>
2023-07-25 10:59:55 +0300

    add Task2

---
+++ задача 2(списки).py
@@ -0,0 +1,6 @@
+first_nums = [10.5, 'sfd', None]
+second_nums = [False, 29]  # создание 2 списков
+
+all_nums = first_nums + second_nums  # объединение списков
+
+print(first_nums.__add__(second_nums))  # объединение маг методом
