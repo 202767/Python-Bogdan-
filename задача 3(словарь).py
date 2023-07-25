e5a6d6d6551cf77f5b55375db1233014c7931392
Anna Chesnokova <chesnokova33@list.ru>
2023-07-25 11:05:27 +0300

    add task3

---
+++ задача 3(словарь).py
@@ -0,0 +1,13 @@
+my_dict = {}
+
+key_name_1 = input('Enter key: ')
+meaning_name_1 = input('Enter meaning: ')
+key_name_2 = input('Enter key: ')
+meaning_name_2 = input('Enter meaning: ')
+key_name_3 = input('Enter key: ')
+meaning_name_3 = input('Enter meaning: ')
+my_dict[key_name_1] = meaning_name_1
+my_dict[key_name_2] = meaning_name_2
+my_dict[key_name_3] = meaning_name_3
+
+print(my_dict)
