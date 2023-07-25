dab3fd5cc98fc01466d19a3bee5571ca3434f8ef
Anna Chesnokova <chesnokova33@list.ru>
2023-07-25 11:11:22 +0300

    add Task 10

---
+++ задача 10(операторы).py
@@ -0,0 +1,7 @@
+first_set = {'mama', 'papa', 'dada'}
+second_set = {'mama', 'papa', 'dada'}
+
+print(first_set == second_set)
+print(first_set is second_set)
+print('mama' in second_set)
+print('baba' in first_set)
